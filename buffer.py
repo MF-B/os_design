956d00f30c7fa5b23db88c77a89a1b91668a3c22
mf1bzz-laptop <guzemin@hotmail.com>
2025-05-26 14:44:46 +0800

    feat: 为Buffer添加`get`方法

--- buffer.py
+++ buffer.py
@@ -6,3 +6,8 @@ class Buffer:
         self.data = Queue(maxsize = size)
     def put(self, data):
         self.data.put(data)
+    def get(self):
+        if not self.data.empty():
+            return self.data.get()
+        return None
+
