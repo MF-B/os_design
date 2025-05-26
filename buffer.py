0ecf199ee34e05ed5c9139c51fefe69f7e1a337d
payafa <1541757897@qq.com>
2025-05-26 15:25:01 +0800

    新增buffer类move

--- buffer.py
+++ buffer.py
@@ -8,6 +8,9 @@ class Buffer:
         if type(data) is str and len(data) == 1:
             self.data.put(data)
         else: raise TypeError('data must be char')
+    def move(self,buffer):
+        temp = buffer.data.put()
+        self.data.get(temp)
     def get(self):
         if not self.data.empty():
             return self.data.get()
